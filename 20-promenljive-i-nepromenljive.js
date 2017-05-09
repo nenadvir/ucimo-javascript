18a0907cab59c6bd2f1b2e7edc4922f0699fe0f0
Damjan Pavlica <mudroljub@gmail.com>
2017-05-09 16:04:18 +0200

    'init'

---
+++ 20-promenljive-i-nepromenljive.js
@@ -0,0 +1,13 @@
+// promenljive se mogu menjati
+
+let promenljivaOdluka = "Danas ću naučiti nešto novo!"
+promenljivaOdluka = "Ipak neću danas ništa učiti"
+
+console.log(promenljivaOdluka)
+
+// nepromenljive se ne mogu menjati
+
+const cvrstaOdluka = "Danas ću naučiti nešto novo!"
+cvrstaOdluka = "Ipak neću danas ništa učiti"
+
+console.log(cvrstaOdluka)
---
+++ 30-tipovi-podataka.js
@@ -0,0 +1,24 @@
+// logicki (bulovi) tipovi
+
+const istina = true
+const laz = false
+
+// broj
+
+const dnevniPazar = 30000
+
+// tekst odnosno struna
+
+const pozdrav = 'Zdravo svete'
+
+// niz
+
+const nizBrojeva = [1, 2, 3, 4, 5]
+
+// objekat
+
+const stolica = {
+  brojNogu: 4,
+  nova: false,
+  opis: "Drvena stolica sa naslonom"
+}
