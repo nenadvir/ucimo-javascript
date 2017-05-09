993e08b18669cce5b90ee0ca5f277efe2a685050
Damjan Pavlica <mudroljub@gmail.com>
2017-05-09 23:38:38 +0200

    tipovi

--- 20-promenljive-i-nepromenljive.js
+++ 20-promenljive-i-nepromenljive.js
@@ -8,6 +8,6 @@ console.log(promenljivaOdluka)
 // nepromenljive se ne mogu menjati
 
 const cvrstaOdluka = "Danas ću naučiti nešto novo!"
-cvrstaOdluka = "Ipak neću danas ništa učiti"
+cvrstaOdluka = "Ipak neću danas ništa učiti"  // ovde puca program
 
 console.log(cvrstaOdluka)
