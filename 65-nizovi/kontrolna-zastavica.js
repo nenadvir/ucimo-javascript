8dc38b2b7b7c407f9758ae9142a31fb2233df782
Damjan Pavlica <mudroljub@gmail.com>
2017-10-02 12:33:07 +0200

    Update kontrolna-zastavica.js

--- 65-nizovi/kontrolna-zastavica.js
+++ 65-nizovi/kontrolna-zastavica.js
@@ -1,6 +1,5 @@
 const imena = ["dajana", "marija", "damjan", "mirko", "ana", "mirko", "dajana", "milena", "darko"]
 const trazenoIme = prompt("Unesi ime")
-
 let nadjeno = false
 
 for (let i = 0; i < imena.length; i++) {
