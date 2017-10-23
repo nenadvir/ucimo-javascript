6eac9320b165a6efd9eab7672ab8d6a02f36158e
mudroljub <mudroljub@gmail.com>
2017-10-23 12:32:14 +0200

    m

---
+++ 82-odnos-html-dom/stablo.js
@@ -0,0 +1,35 @@
+const html = {
+  head: {
+    meta: {
+      charset: "UTF-8"
+    },
+    meta: {
+      name: "viewport",
+      content: "width=device-width"
+    },
+    title: {
+      sadrzaj: "HTML stablo"
+    }
+  },
+  body: {
+    div: {
+      h1: {
+        sadrzaj: "Dobrodošli"
+      },
+      img: {
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Heckert_GNU_white.svg/64px-Heckert_GNU_white.svg.png"
+      },
+      p: {
+        sadrzaj: "HTML struktura je razgranata. Deca elementi se nalaze unutar roditeljskih elemenata."
+      }
+    },
+    div: {
+      h2: {
+        sadrzaj: "Učimo web development"
+      },
+      p: {
+        sadrzaj: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus sint, necessitatibus officia officiis consequuntur doloribus impedit, optio quaerat, repellendus veniam placeat velit ex sapiente ullam iusto quis. Quasi, alias, quod."
+      }
+    }
+  }
+}
