5b5482e26cbcdc727b412fab8571c3955fdd5758
Damjan Pavlica <mudroljub@gmail.com>
2017-10-23 20:49:20 +0200

    cistka

--- 80-dom-manipulacija/main.js
+++ 80-dom-manipulacija/main.js
@@ -1,11 +1,15 @@
-function $ (selektor) {
-    return document.querySelector(selektor)
+// alias funkcija
+
+function $(selektor) {
+  return document.querySelector(selektor)
 }
 
-const print = console.log
+// menjamo stil cele stranice
 
 document.body.style.background = 'red';
 document.body.style.fontSize = '1.2em';
+
+// selektujemo pojedine elemente i menjamo ih
 
 const naslov = document.getElementById('naslov')
 naslov.innerText = 'Zdravo Zdravo'
@@ -13,9 +17,9 @@ naslov.innerText = 'Zdravo Zdravo'
 const prazno = document.getElementById('prazno')
 prazno.innerHTML = `
 <div>
-    <h3>Promenjen naslov</h3>
-    <p>asdf asdflk asdfl ksdflkj </p>
-    <p>asdf asdflk asdfl ksdflkj </p>
+    <h3>Dodat naslov</h3>
+    <p>Dodat pasus asdf asdflk asdfl ksdflkj </p>
+    <p>Dodat pasus asdf asdflk asdfl ksdflkj </p>
     <img>
 </div>
 `
@@ -24,8 +28,10 @@ $('#naslov').style.color = 'green'
 $('.prvi-pasus').style.color = 'yellow'
 $('h3').style.color = 'purple'
 
+// selektujemo više elemenata i stampamo ih
+
 const pasusi = document.querySelectorAll('p')
 
 for (let i = 0; i < pasusi.length; i++) {
-    print (pasusi[i])
+  console.log(pasusi[i])
 }
