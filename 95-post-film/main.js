9558eea0b958a26dd2942699d61ac4924292bb09
Damjan Pavlica <mudroljub@gmail.com>
2017-11-10 23:46:12 +0100

    Update main.js

--- 95-post-film/main.js
+++ 95-post-film/main.js
@@ -1,53 +1,15 @@
-// FILMOVI
-
-var $ = function (selector) {
-    return document.querySelector(selector);
-};
-
-var poljeFilm = $('#dodajFilm');
-var poljeGodina = $('#dodajGodinu');
-var poljeUrl = $('#dodajUrl')
-var dugme = $('#dugmic');
-var bigBox = $('#big_box');
+const forma = document.querySelector('form')
+const {naziv, godina, slika} = forma.elements // izvlaci elemente iz forme
 
 function posalji(e) {
-    e.preventDefault();
+  e.preventDefault()
 
-    var xhr = new XMLHttpRequest();
-    var url = "https://baza-filmova.herokuapp.com/dodaj/";
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log(xhr.responseText);
-        }
-    };
+  const http = new XMLHttpRequest()
+  http.open('POST', 'https://baza-filmova.herokuapp.com/dodaj/')
+  http.setRequestHeader('Content-type', 'application/json')
+  http.onload = () => console.log(http.responseText)
 
-    var parametri = {
-        "naziv": poljeFilm.value,
-        "godina": poljeGodina.value,
-        "slika": poljeUrl.value
-    }
-    console.log(parametri);
-    var data = JSON.stringify(parametri);
-
-    xhr.send(data);
-    console.log(data);
+  http.send(JSON.stringify({'naziv': naziv.value, 'godina': godina.value, 'slika': slika.value}))
 }
 
-dugme.addEventListener('click', addFilm);
-dugme.addEventListener('click', posalji);
-
-function prikazi(e) {
-    e.preventDefault();
-    function loadDoc() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("bigBox").innerHTML = this.responseText;
-            }
-        };
-        xhttp.open("GET", "demo_get.asp", true);
-        xhttp.send();
-    }
-}
+forma.addEventListener('submit', posalji)
