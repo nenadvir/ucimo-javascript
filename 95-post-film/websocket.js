83ab5275e7edf8c538137ed1e5aa2270754148c7
Damjan Pavlica <mudroljub@gmail.com>
2017-11-11 01:35:13 +0100

    Create websocket.js

---
+++ 95-post-film/websocket.js
@@ -0,0 +1,16 @@
+const ws = new WebSocket('ws://baza-filmova.herokuapp.com')
+const forma = document.querySelector('form')
+const {naziv, godina, slika} = forma.elements // izvlaci elemente iz forme
+
+forma.addEventListener('submit', e => {
+  e.preventDefault()
+  const http = new XMLHttpRequest()
+  http.open('POST', 'http://baza-filmova.herokuapp.com/dodaj/')
+  http.setRequestHeader('Content-type', 'application/json')
+  http.onload = () => console.log(http.responseText)
+  http.send(JSON.stringify({'naziv': naziv.value, 'godina': godina.value, 'slika': slika.value}))
+})
+
+ws.addEventListener('message', e => {
+  console.log('Server javlja: ' + e.data)
+})
