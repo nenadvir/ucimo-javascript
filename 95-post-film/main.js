5032960401b7b1d4cffff852df01788560cb9e5e
Damjan Pavlica <mudroljub@gmail.com>
2017-11-11 01:34:16 +0100

    Update main.js

--- 95-post-film/main.js
+++ 95-post-film/main.js
@@ -1,15 +1,11 @@
 const forma = document.querySelector('form')
 const {naziv, godina, slika} = forma.elements // izvlaci elemente iz forme
 
-function posalji(e) {
+forma.addEventListener('submit', e => {
   e.preventDefault()
-
   const http = new XMLHttpRequest()
-  http.open('POST', 'https://baza-filmova.herokuapp.com/dodaj/')
+  http.open('POST', 'http://baza-filmova.herokuapp.com/dodaj/')
   http.setRequestHeader('Content-type', 'application/json')
   http.onload = () => console.log(http.responseText)
-
   http.send(JSON.stringify({'naziv': naziv.value, 'godina': godina.value, 'slika': slika.value}))
-}
-
-forma.addEventListener('submit', posalji)
+})
